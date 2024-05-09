48f8383dba01eb6dd3c29c06892fd7dde17a2575
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-05-09 18:59:43 +0300

    main

--- App530/ContentView.swift
+++ App530/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var current_tab: Tab = Tab.Result
+
+    @AppStorage("status") var status: Bool = false
+    
+    init() {
+        
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Color.black
+                .ignoresSafeArea()
+            
+            if status {
+            
+            VStack(spacing: 0, content: {
+            
+                    TabView(selection: $current_tab, content: {
+                        
+                        ResultView()
+                            .tag(Tab.Result)
+                        
+                        CombinationsView()
+                            .tag(Tab.Combinations)
+                        
+                        PopularView()
+                            .tag(Tab.Popular)
+                        
+                        NotesView()
+                            .tag(Tab.Notes)
+                        
+                        SettingsView()
+                            .tag(Tab.Settings)
+                        
+                    })
+                    
+                    TabBar(selectedTab: $current_tab)
+                })
+                    .ignoresSafeArea(.all, edges: .bottom)
+                    .onAppear {
+                        
+                    }
+                
+            } else {
+                
+                R1()
+            }
         }
-        .padding()
     }
 }
 
